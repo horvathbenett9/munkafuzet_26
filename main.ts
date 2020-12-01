1a69ae98b2ce3dc0bacfd12b1cbf061d5453081b
horvathbenett9 <75091388+horvathbenett9@users.noreply.github.com>
2020-12-01 17:57:56 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,85 +1,87 @@
 input.onButtonPressed(Button.A, function () {
     basic.clearScreen()
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . #
-        . . . . .
-        . . . . .
-        `)
-    basic.pause(100)
-    basic.showLeds(`
-        . . . . .
-        . . . . #
-        . . . # #
-        . . . . #
-        . . . . .
-        `)
-    basic.pause(100)
-    basic.showLeds(`
-        . . . . #
-        . . . # .
-        . . # # #
-        . . . # .
-        . . . . #
-        `)
-    basic.pause(100)
-    basic.showLeds(`
-        . . . # .
-        . . # . .
-        . # # # #
-        . . # . .
-        . . . # .
-        `)
-    basic.pause(100)
-    basic.showLeds(`
-        . . # . .
-        . # . . .
-        # # # # #
-        . # . . .
-        . . # . .
-        `)
-    basic.pause(100)
-    basic.showLeds(`
-        . # . . .
-        # . . . .
-        # # # # .
-        # . . . .
-        . # . . .
-        `)
-    basic.pause(100)
-    basic.showLeds(`
-        # . . . .
-        . . . . .
-        # # # . .
-        . . . . .
-        # . . . .
-        `)
-    basic.pause(100)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        # # . . .
-        . . . . .
-        . . . . .
-        `)
-    basic.pause(100)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        # . . . .
-        . . . . .
-        . . . . .
-        `)
-    basic.pause(100)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-    basic.pause(100)
+    for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . #
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . . . .
+            . . . . #
+            . . . # #
+            . . . . #
+            . . . . .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . . . #
+            . . . # .
+            . . # # #
+            . . . # .
+            . . . . #
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . . # .
+            . . # . .
+            . # # # #
+            . . # . .
+            . . . # .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . # . . .
+            # . . . .
+            # # # # .
+            # . . . .
+            . # . . .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            # # # . .
+            . . . . .
+            # . . . .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(100)
+    }
 })
 basic.forever(function () {
 	
